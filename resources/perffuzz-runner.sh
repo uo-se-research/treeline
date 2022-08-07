7ead37f1afd58d38cb5f82070328492e442c0ae1
Ziyad Alsaeed <zalsaeed@cs.uoregon.edu>
2022-08-07 16:58:17 +0300

    fixed wf configurations and better message handling for slack

--- resources/perffuzz-runner.sh
+++ resources/perffuzz-runner.sh
@@ -11,7 +11,7 @@
 
 declare -a benchmarks=(
   # "libxml2/perffuzz-seeds/60/;libxml2;/home/treeline/target_apps/libxml2/src/libxml2-2.9.7/xmllint @@"
-  # "word-frequency/perffuzz-seeds/60/;wf/home/treeline/target_apps/word-frequency/src/wf-0.41/src/wf"
+  # "word-frequency/perffuzz-seeds/60/;wf;/home/treeline/target_apps/word-frequency/src/wf-0.41/src/wf"
   # "graphviz/perffuzz-seeds/60/;graphviz;dot"
   # "flex/perffuzz-seeds/;flex;/home/treeline/target_apps/flex/src/flex-2.6.4/src/flex"
   # "lunasvg/inputs;lunasvg;svg2png @@"
@@ -51,7 +51,7 @@ do
     command="afl-fuzz -i /home/treeline/target_apps/${seeds} -o ${outputlocation} -p -t ${input_timeout} -N ${budget} -d ${bin_location}"
     
     # inform us on what is about to run and where.
-    python3 perffuzz-reporting.py m "PerfFuzz running new experiment on ${app_name} for ${duration} hours."
+    python3 perffuzz-reporting.py m "PerfFuzz running new experiment on \`${app_name}\` for \`${duration}\` hours."
     python3 perffuzz-reporting.py m "Exact command is ..."
     python3 perffuzz-reporting.py c "timeout ${duration}h ${command}"
     
@@ -77,9 +77,9 @@ do
     input_content=$(<$inputname)
 
     # inform us on all the experiment essential results.
-    python3 perffuzz-reporting.py m "Run found in ${outputlocation} just finished!"
-    python3 perffuzz-reporting.py m "Max found input location/name: ${inputname}"
-    python3 perffuzz-reporting.py m "Max found input cost: ${max_cost}"
+    python3 perffuzz-reporting.py m "Run found in \`${outputlocation}\` just finished!"
+    python3 perffuzz-reporting.py m "Max found input location/name: \`${inputname}\`"
+    python3 perffuzz-reporting.py m "Max found input cost: \`${max_cost}\`"
     python3 perffuzz-reporting.py m "Max found input content:"
     python3 perffuzz-reporting.py c "${input_content}"
     python3 perffuzz-reporting.py m "The following is the run statistics"
